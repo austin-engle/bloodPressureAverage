94cfa9a6d52227019157a90df2dec85b7fd08fcf
austin-engle-stu <aengl29@wgu.edu>
2019-12-25 18:32:24 -0700

    Added inforation to top of app.py that is also found in README.md to follow PEP8 standards

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
 import functions
 
+"""
+    I created this app to help gather and average blood pressure. 
+
+    This app will walk someone through taking their 
+        blood pressure three times and will find the 
+        average of the following: SYS, DIA, PUL. 
+
+    All of the code lives in funcitions.py and is called from app.py.
+
+    This was done as a way to practice importing libraries and passing 
+        in variable values. 
+"""
+
 # Clears the console
 functions.clear()
 
