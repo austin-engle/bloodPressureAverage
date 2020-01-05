3341c8c6d82d5e05fb99efc0775b170621b83904
austin-engle <aengl29@wgu.edu>
2020-01-05 13:59:28 -0700

    commit after blood pressure reading: 1/5/2020 01:58 PM

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ ninty_day_average = functions.average_over_time(90)
 all_time_average = functions.all_time_average()
 
 # Save all averages calculated to a file
-functions.write_averages_to_file(seven_day_average, thirty_day_average, ninty_day_average, all_time_average)
+functions.write_averages_to_file(avg_bp, seven_day_average, thirty_day_average, ninty_day_average, all_time_average)
 
 # Push all updates to GitHub
 functions.push_to_github()
--- functions.py
+++ functions.py
@@ -24,7 +24,7 @@ def reading(num):
     # Gathers the blood pressure readings.
 
     print(f"Please take the {num} reading now.\n")
-    sleep(30)
+    # sleep(30)
 
     correct = None
     sys = None
@@ -46,11 +46,7 @@ def reading(num):
 
         print("")
 
-    bp = {
-        "sys": sys,
-        "dia": dia,
-        "pul": pul
-    }
+    bp = {"sys": sys, "dia": dia, "pul": pul}
 
     return bp
 
@@ -64,7 +60,7 @@ def countdown():
 
     while countdown != 0:
         countdown = countdown - 1
-        sleep(1)
+        # sleep(1)
         if countdown == 20 or countdown == 10:
             print(f"{countdown} seconds remaining.")
 
@@ -95,7 +91,7 @@ def diagnosis_output(avg_bp):
 
     if avg_bp["avg_sys"] < 90:
         print(f"Your average sys is showing signs of Hypotension")
-        sys_diagnosis = '1'
+        sys_diagnosis = "1"
 
     elif avg_bp["avg_sys"] >= 91 and avg_bp["avg_sys"] <= 120:
         print(f"Your average sys is in a healthy range")
@@ -117,7 +113,7 @@ def diagnosis_output(avg_bp):
 
     if avg_bp["avg_dia"] < 60:
         print(f"Your average dia is showing signs of Hypotension")
-        dia_diagnosis = '1'
+        dia_diagnosis = "1"
 
     elif avg_bp["avg_dia"] >= 61 and avg_bp["avg_dia"] <= 80:
         print(f"Your average dia is in a healthy range")
@@ -144,10 +140,10 @@ def diagnosis_output(avg_bp):
         "2": "Normal",
         "3": "Prehypertension",
         "4": "Stage 1 Hypertension",
-        "5": "Stage 2 Hypertension"
+        "5": "Stage 2 Hypertension",
     }
 
-    if sys_diagnosis == '1' or dia_diagnosis == '1':
+    if sys_diagnosis == "1" or dia_diagnosis == "1":
         diagnosis = dia["1"]
 
     elif sys_diagnosis > dia_diagnosis:
@@ -162,26 +158,24 @@ def diagnosis_output(avg_bp):
 
 
 def tags():
-    tags_arm = {
-        '1': 'Right arm',
-        '2': 'Left arm'
-    }
+    tags_arm = {"1": "Right arm", "2": "Left arm"}
 
-    tags_position = {
-        '1': "Seated",
-        '2': "Laying Down"
-    }
+    tags_position = {"1": "Seated", "2": "Laying Down"}
 
     arm_choice = None
     position_choice = None
 
-    while arm_choice != '1' and arm_choice != '2':
-        arm_choice = input('Did you take the reading with your Right(1) or Left(2) arm?\n')
+    while arm_choice != "1" and arm_choice != "2":
+        arm_choice = input(
+            "Did you take the reading with your Right(1) or Left(2) arm?\n"
+        )
 
-    while position_choice != '1' and position_choice != '2':
-        position_choice = input('Were you seated(1) or laying down(2) when taking the reading?\n')
+    while position_choice != "1" and position_choice != "2":
+        position_choice = input(
+            "Were you seated(1) or laying down(2) when taking the reading?\n"
+        )
 
-    tags = f'{tags_arm[arm_choice]} | {tags_position[position_choice]}'
+    tags = f"{tags_arm[arm_choice]} | {tags_position[position_choice]}"
 
     return tags
 
@@ -192,11 +186,13 @@ def push_to_github():
     sleep(5)
     os.system("git add * >/dev/null 2>&1")
     sleep(2)
-    os.system(f"git commit -m 'commit after blood pressure reading: {date} {time_12hr}' >/dev/null 2>&1")
+    os.system(
+        f"git commit -m 'commit after blood pressure reading: {date} {time_12hr}' >/dev/null 2>&1"
+    )
     sleep(2)
     os.system(f"git push >/dev/null 2>&1")
 
-    print('Pushed to git successfully, exiting script')
+    print("Pushed to git successfully, exiting script")
 
 
 def write_to_csv(avg_bp, diagnosis, tags):
@@ -204,13 +200,13 @@ def write_to_csv(avg_bp, diagnosis, tags):
     # rows to gather
 
     # SYS
-    sys = avg_bp['avg_sys']
+    sys = avg_bp["avg_sys"]
 
     # DIA
-    dia = avg_bp['avg_dia']
+    dia = avg_bp["avg_dia"]
 
     # PUL
-    pul = avg_bp['avg_pul']
+    pul = avg_bp["avg_pul"]
 
     # BPZ
     bpz = diagnosis
@@ -222,7 +218,8 @@ def write_to_csv(avg_bp, diagnosis, tags):
     csv_write_format = date, time_12hr, sys, dia, pul, bpz, tags
 
     import csv
-    with open(r'bloodpressure.csv', 'a') as f:
+
+    with open(r"bloodpressure.csv", "a") as f:
         writer = csv.writer(f)
         writer.writerow(csv_write_format)
 
@@ -230,13 +227,14 @@ def write_to_csv(avg_bp, diagnosis, tags):
 def average_over_time(t):
 
     # Calculate the average blood pressure over time from the csv file
+    sleep(4)
 
     sys_data = []
     dia_data = []
     pul_data = []
 
     for i in range(t):
-        n = str(now - datetime.timedelta(days=i)).replace('-', '/')
+        n = str(now - datetime.timedelta(days=i)).replace("-", "/")
 
         year = n[0:4]
         month = n[5:7]
@@ -244,10 +242,10 @@ def average_over_time(t):
 
         adj_date = f"{month}/{day}/{year}"
 
-        with open('bloodpressure.csv', newline='') as csvfile:
+        with open("bloodpressure.csv", newline="") as csvfile:
             csv_data = csv.DictReader(csvfile)
             for row in csv_data:
-                if row['DATE'] == adj_date:
+                if row["DATE"] == adj_date:
                     sys = int(row["SYS"])
                     dia = int(row["DIA"])
                     pul = int(row["PUL"])
@@ -258,7 +256,7 @@ def average_over_time(t):
     avg_data = {
         "sys": round(mean(sys_data)),
         "dia": round(mean(dia_data)),
-        "pul": round(mean(pul_data))
+        "pul": round(mean(pul_data)),
     }
 
     return avg_data
@@ -272,7 +270,7 @@ def all_time_average():
     dia_data = []
     pul_data = []
 
-    with open('bloodpressure.csv', newline='') as csvfile:
+    with open("bloodpressure.csv", newline="") as csvfile:
         csv_data = csv.DictReader(csvfile)
         for row in csv_data:
             # if row['DATE'] == adj_date:
@@ -286,19 +284,26 @@ def all_time_average():
     avg_data = {
         "sys": round(mean(sys_data)),
         "dia": round(mean(dia_data)),
-        "pul": round(mean(pul_data))
+        "pul": round(mean(pul_data)),
     }
 
     return avg_data
 
 
-def write_averages_to_file(seven_day_average, thirty_day_average, ninty_day_average, all_time_average):
+def write_averages_to_file(
+    avg_bp, seven_day_average, thirty_day_average, ninty_day_average, all_time_average
+):
 
     # Writes the averages to a file for easy tracking
 
+    latest_reading_message = f"Latest Reading: {avg_bp}"
     seven_day_average_message = f'7 Day Average: {seven_day_average["sys"]}/{seven_day_average["dia"]} {seven_day_average["pul"]}'
     thirty_day_average_message = f'30 Day Average: {thirty_day_average["sys"]}/{thirty_day_average["dia"]} {thirty_day_average["pul"]}'
     ninty_day_average_message = f'90 Day Average: {ninty_day_average["sys"]}/{ninty_day_average["dia"]} {ninty_day_average["pul"]}'
     all_time_average_message = f'All Time Average: {all_time_average["sys"]}/{all_time_average["dia"]} {all_time_average["pul"]}'
 
-    print(f"{seven_day_average_message}\n{thirty_day_average_message}\n{ninty_day_average_message}\n{all_time_average_message}", file=open('averages.txt', 'w'))
+    print(
+        f"{latest_reading_message}\n{seven_day_average_message}\n{thirty_day_average_message}\n{ninty_day_average_message}\n{all_time_average_message}",
+        file=open("averages.txt", "w"),
+    )
+
