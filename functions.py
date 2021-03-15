4740719e4d43a42d5b064f352f61f750dc58cd57
austin-engle <aengl29@wgu.edu>
2021-03-15 11:30:35 -0600

    updated script

--- functions.py
+++ functions.py
@@ -36,7 +36,7 @@ def reading(num):
     # Gathers the blood pressure readings.
 
     print(f"Please take the {num} reading now.\n")
-    # sleep(30)
+    sleep(30)
 
     correct = None
     sys = None
@@ -51,7 +51,7 @@ def reading(num):
         pul = int(input(f"What is the PUL of the {num} reading? "))
 
         print(f"\n{num.capitalize()} Blood Pressure reading:\n {sys}/{dia} {pul}\n")
-        # sleep(1)
+        sleep(1)
 
         correct = input("Is this the correct blood pressure reading? (Yes/No) ").lower()
 
@@ -71,7 +71,7 @@ def countdown():
 
     while countdown != 0:
         countdown = countdown - 1
-        # sleep(1)
+        sleep(1)
         if countdown == 20 or countdown == 10:
             print(f"{countdown} seconds remaining.")
 
@@ -83,7 +83,7 @@ def avgbp(bp1, bp2, bp3):
     # Averages the Blood Pressures that were previously collected.
 
     print(f"Calculating average blood pressure")
-    # sleep(2)
+    sleep(2)
 
     avg_sys = int(((bp1["sys"] + bp2["sys"] + bp3["sys"]) / 3))
     avg_dia = int(((bp1["dia"] + bp2["dia"] + bp3["dia"]) / 3))
@@ -194,13 +194,13 @@ def tags():
 def push_to_github():
 
     # Pushes the updates to github automatically as the end of the script
-    # sleep(3)
+    sleep(3)
     os.system("git add * >/dev/null 2>&1")
-    # sleep(2)
+    sleep(2)
     os.system(
         f"git commit -m 'commit after blood pressure reading: {date} {time_12hr}' >/dev/null 2>&1"
     )
-    # sleep(2)
+    sleep(2)
     os.system(f"git push >/dev/null 2>&1")
 
     print("Pushed to git successfully, exiting script")
